3f59cd2ced9de0cfb0a3d0176c1c931541264c00
prayoga.triady <prayoga.triady@indocyber.id>
2023-11-03 16:40:21 +0700

    add user_id to voucher

--- migration/table_m_vouchers.sql
+++ migration/table_m_vouchers.sql
@@ -5,6 +5,7 @@ CREATE TABLE m_vouchers (
     quantity_all INT NOT NULL,
     quantity_user INT NOT NULL,
     expired_at TIMESTAMP NOT NULL,
+    user_id BIGINT NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -12,3 +13,6 @@ CREATE TABLE m_vouchers (
     INDEX (id),
     PRIMARY KEY (id)
 );
+
+ALTER TABLE m_vouchers
+ADD CONSTRAINT FK1_Users_Vouchers FOREIGN KEY (user_id) REFERENCES m_users(id);
